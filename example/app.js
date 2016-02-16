bdaf2de7850f656a460582be651a60837b4a180a
Hattan Shobokshi <hattan@gmail.com>
2016-02-16 01:06:26 -0800

    added example

---
+++ example/app.js
@@ -0,0 +1,22 @@
+$(function(){
+
+	//initialize bond
+	var model = new bond.Model();
+	bond.bind(model);
+
+	//bind to a div
+	model.message = "hello world!";
+	
+  //bind to an input
+	model.random_input = "some value";
+
+  //bind to an array and use a handlebars templates (see markup)
+	var data = [
+		{id : 0, name : "bob"},
+		{id : 1, name : "mike"},
+		{id : 2, name : "sally"},
+		{id : 3, name : "jennifer"}
+	];
+	model.items = data;
+
+});
